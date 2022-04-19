28b65701fd41391941b68ad75038aba5a72db0bd
Claudio Ferreira <claudio.ferreira@aizoon.it>
2022-04-19 17:17:46 +0200

    entrega desafio

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,8 +1,16 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
+import { format, minutesToHours } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { SpinningCircles } from 'react-loading-icons';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
-import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 interface Post {
@@ -26,20 +34,154 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+interface Content {
+  heading: string;
+  body: {
+    text: string;
+  }[];
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+export default function Post({ post }: PostProps): JSX.Element {
+  const { isFallback } = useRouter();
 
-//   // TODO
-// };
+  if (isFallback) {
+    return (
+      <div className={styles.loading}>
+        <span>Carregando...</span>
+        <SpinningCircles stroke="#784be1" className={styles.icon} />
+      </div>
+    );
+  }
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+  const calculateReadingTime = (content: Content[]): string => {
+    const getHeadingWordsPerMinutes = content.reduce((acc, currentValue) => {
+      return currentValue.heading.split(/\s+/).length + acc;
+    }, 0);
 
-//   // TODO
-// };
+    const getBodyWordsPerMinutes = content.reduce((acc, currentValue) => {
+      return RichText.asText(currentValue.body).split(/\s+/).length + acc;
+    }, 0);
+
+    const getWordsPerMinutes = Math.ceil(
+      (getHeadingWordsPerMinutes + getBodyWordsPerMinutes) / 200
+    );
+
+    if (getWordsPerMinutes < 1) {
+      return 'Rápida leitura';
+    }
+
+    if (getWordsPerMinutes < 60) {
+      return `${getWordsPerMinutes} min`;
+    }
+
+    return `${minutesToHours(getWordsPerMinutes)} horas`;
+  };
+
+  const formatDate = (date: string): string => {
+    return format(new Date(date), 'dd MMM yyyy', { locale: ptBR });
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{post.data.title} | Spacetraveling</title>
+      </Head>
+
+      <Header />
+
+      {post.data.banner && (
+        <img
+          className={styles.banner}
+          src={post.data.banner.url}
+          alt="banner"
+        />
+      )}
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <header>
+            <h1>{post.data.title}</h1>
+            <div className={styles.postInfo}>
+              <span>
+                <FiCalendar color="#BBBBBB" size={20} />
+                {formatDate(post.first_publication_date)}
+              </span>
+              <span>
+                <FiUser color="#BBBBBB" size={20} />
+                {post.data.author}
+              </span>
+              <span>
+                <FiClock color="#BBBBBB" size={20} />
+                {calculateReadingTime(post.data.content)}
+              </span>
+            </div>
+          </header>
+
+          <section className={styles.postContent}>
+            {post.data.content.map(content => (
+              <div key={content.heading}>
+                <h1>{content.heading}</h1>
+                <div
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{
+                    __html: RichText.asHtml(content.body),
+                  }}
+                />
+              </div>
+            ))}
+          </section>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.slug'],
+    }
+  );
+
+  const params = posts.results.map(post => ({
+    params: { slug: post.uid },
+  }));
+
+  return {
+    paths: params,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('repeatable', String(slug), {});
+
+  const postData = {
+    data: {
+      title: response.data.title,
+      subtitle: response.data.subtitle,
+      banner: {
+        url: response.data.banner.url ?? '',
+      },
+      author: response.data.author,
+      content: response.data.content.map(content => ({
+        heading: content.heading,
+        body: content.body,
+      })),
+    },
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
+  } as Post;
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  };
+};
